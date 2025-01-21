948345482f282f786fda7e4b22584e6f0af609e8
Julen <j.aguero.fernandez05@gmail.com>
2025-01-21 23:41:56 +0100

    new fonts added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      // * FUENTES 
+      fontFamily: {
+        Inter: ["Inter", "sans-serif"],
+        Poppins: ["Poppins", "sans-serif"],
+        WorkSans: ["Work Sans", "sans-serif"],
+        NotoSans: ["Noto Sans", "sans-serif"],
+        Roboto: ["Roboto", "sans-serif"],
+        Rubik: ["Rubik", "sans-serif"],
+        Montserrat: ["Montserrat", "sans-serif"],
+      },
+      fontSize: {
+        'trece': '13px',
+      }
+    },
   },
   plugins: [],
 }
