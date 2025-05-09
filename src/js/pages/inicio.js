f446dc0e3f5f76214b407e44b49a139c505e9aaf
Julen <j.aguero.fernandez05@gmail.com>
2025-05-09 16:15:59 +0200

    home file

---
+++ src/js/pages/inicio.js
@@ -0,0 +1,8 @@
+
+const total_notis = document.getElementById("total_notis")
+const usuario_nombre = document.getElementById("usuario_nombre")
+const usuario_nombre_canal = document.getElementById("usuario_nombre_canal")
+
+
+
+
