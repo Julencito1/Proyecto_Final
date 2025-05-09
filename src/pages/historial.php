b7be877e5b3a36915dcccbecda7c4275e3362b4c
Julen <j.aguero.fernandez05@gmail.com>
2025-05-09 16:16:17 +0200

    history page

---
+++ src/pages/historial.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Historial - NewTube</title>
+  <link href='../styles/styles.css' rel='stylesheet'>
+  <link href="../output.css" rel="stylesheet">
+  <link rel="icon" type="image/x-icon" href="../assets/favicon/newtube.ico">
+  <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body>
+  <div id="plantilla">
+    <?php include '../components/header.php'; ?>
+    <main id='main'>
+dsdsa
+    </main>
+    <?php include '../components/sidebar.php'; ?>
+  </div>
+  <script src="../js/components/sidebar.js"></script>
+  <script src="../js/components/header.js"></script>
+  <script src="../js/utils/observer.js"></script>
+  <script src="../js/main.js"></script>
+  <script src="../js/animaciones/animaciones.js"></script>
+
+</body>
+</html>
