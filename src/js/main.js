c2625fa2b516804a090c8d64a6633df0df61ad7f
Julen <j.aguero.fernandez05@gmail.com>
2025-06-02 23:13:12 +0200

    handle new menus and recent searches localstorage data

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,10 @@ const contenedor_notificaciones_btn = document.getElementById("contenedor_notifi
 const contenedor_buscador = document.getElementById("contenedor_buscador")
 const contenedor_menu_usuario = document.getElementById("contenedor_menu_usuario")
 const contenedor_crear = document.getElementById("contenedor_crear")
+const contenedor_submenu_privado = document.getElementById("contenedor_submenu_privado")
+const contenedor_filtros_videos_guardar = document.getElementById("contenedor_filtros_videos_guardar")
+const contenedor_submenu_tres_puntos_video = document.getElementById("contenedor_submenu_tres_puntos_video")
+const contenedor_velocidades_video = document.getElementById("contenedor_velocidades_video")
 const main = document.getElementById('main')
 
 document.addEventListener("click", (e) => {
@@ -25,7 +29,30 @@ document.addEventListener("click", (e) => {
   {
     MenuCrearNuevoOut()
   }
+
+  if (contenedor_filtros_videos_guardar && !contenedor_filtros_videos_guardar.contains(e.target))
+  {
+    MenuUsuarioFiltroVideosGuardarOut()
+  }
+
+  if (contenedor_submenu_tres_puntos_video && !contenedor_submenu_tres_puntos_video.contains(e.target))
+  {
+    MostrarMenuTresPuntosVideoOut()
+  }
+  
+  if (contenedor_velocidades_video && !contenedor_velocidades_video.contains(e.target))
+    {
+      VelocidadesVideoOut()
+    }
+
+
 })
 
+let busquedasRecientes = localStorage.getItem("busquedas_recientes")
+
+if (!busquedasRecientes)
+{
+  localStorage.setItem("busquedas_recientes", JSON.stringify([]))
+}
 
 
