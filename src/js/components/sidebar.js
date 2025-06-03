cf50e79d9121ac76083b49a84b5f58af67c62b42
Julen <j.aguero.fernandez05@gmail.com>
2025-06-03 20:36:01 +0200

    removed show more subscribers button

--- src/js/components/sidebar.js
+++ src/js/components/sidebar.js
@@ -1,7 +1,7 @@
 const links_sidebar = [
     `Inicio`,
     `Historial`,
-    `Me Gusta`,
+    `Marcados`,
     `Videos Guardados`,
     `Suscripciones`
 ]
@@ -98,22 +98,6 @@ function SidebarSuscripciones()
                             contenedor_suscripciones.appendChild(nuevoA)
                         }
             
-                        if (datos.mensaje.suscripciones.mostrar)
-                        {
-                            let nuevoB = document.createElement("a")
-            
-                            nuevoB.classList.add("sidebar_nav", "rounded-md", "px-2", "w-full", "h-[34px]", "flex", "items-center", "gap-2", "cursor-pointer", "transition-colors", "duration-150", "hover:bg-[#f2f2f2]")
-                            nuevoB.title = 'Mostrar Más'
-                            nuevoB.innerHTML = `
-                        
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide min-w-[16px] lucide-chevron-down-icon lucide-chevron-down"><path d="m6 9 6 6 6-6"/></svg>
-                            <div id="suscripciones_mm" class="text-[13.2px] font-Roboto line-clamp-1">
-                                Mostrar Más
-                            </div>
-                            
-                            `
-                            contenedor_suscripciones.appendChild(nuevoB)
-                        }
                 }
 
             }
