24200c9925ea82efeb7d82b361203478c60231d3
Julen <j.aguero.fernandez05@gmail.com>
2025-06-05 21:00:01 +0200

    responsive remove menu

--- src/components/header.php
+++ src/components/header.php
@@ -3,7 +3,7 @@
 <div id="header" class="px-4 py-2 flex items-center justify-between">
       <div>
         <div class="flex items-center gap-1">
-          <button aria-label="Botón Menu Sidebar" id="btn_sidebar" class="p-2 rounded-full cursor-pointer transition-colors duration-150 hover:bg-[#f2f2f2]">
+          <button aria-label="Botón Menu Sidebar" id="btn_sidebar" class="p-2 max-md:hidden rounded-full cursor-pointer transition-colors duration-150 hover:bg-[#f2f2f2]">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu-icon lucide-menu"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg>
           </button>
           <a href="/" aria-label="Link Página Principal" class="text-white">
